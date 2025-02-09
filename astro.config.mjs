9683a1ede1193ccf6d8be545461faad5cbfc0757
hello@aviv.sh <38299977+RedYetiDev@users.noreply.github.com>
2025-02-09 16:10:21 -0500

    Update to Tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 const base = "https://aviv.sh";
 
 // https://astro.build/config
 export default defineConfig({
   site: base,
-  integrations: [tailwind(), icon(), sitemap()],
+  integrations: [icon(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
